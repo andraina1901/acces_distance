50d77a4f20cfe2c07cbb1aaacfc29c3a45f21da9
andraina1901 <randraina@yahoo.com>
2022-12-06 07:18:59 -0800

    with show screen

--- MainClient.java
+++ MainClient.java
@@ -1,11 +1,16 @@
 package client;
 
+import java.net.InetAddress;
 import java.net.Socket;
+
+import client.ClientScreen;
 
 public class MainClient {
     public static void main(String[] args) throws Exception {
-
-        Client c = new Client("192.168.10.168");
+        String hostAdress = InetAddress.getLocalHost().getHostAddress();
+        Client c = new Client(hostAdress);
         Socket client = c.getSocket();
+        
+        
     }
 }
